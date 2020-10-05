cfca20583761df4150d9885d3b0b31929aa634b7
Jeremías <jrmsdev@gmail.com>
2020-10-04 23:01:47 -0300

    test main cmd no args

--- cmd/mks/main.go
+++ cmd/mks/main.go
@@ -5,26 +5,34 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	mks "github.com/jrmsdev/gomks"
 )
 
+var exit func(int) = os.Exit
+
 func main() {
-	if len(os.Args) != 2 {
-		mks.Log("ERROR: %s", "invalid args")
-		os.Exit(1)
+	flag.Parse()
+	exit(run(flag.Args()))
+}
+
+func run(args []string) int {
+	if len(args) == 0 {
+		mks.Log("ERROR: %s", "no args")
+		return 1
 	}
-	if os.Args[1] == "version" {
+	if args[0] == "version" {
 		mks.Log("mks version %s", mks.Version())
-		os.Exit(0)
+		return 0
 	}
 	e := mks.NewEnv()
-	for _, fn := range os.Args[1:] {
+	for _, fn := range args {
 		if err := mks.Eval(e, fn); err != nil {
 			mks.Log("ERROR: %s: %v", fn, err)
-			os.Exit(2)
+			return 2
 		}
 	}
-	os.Exit(0)
+	return 0
 }
--- cmd/mks/main_test.go
+++ cmd/mks/main_test.go
@@ -4,11 +4,37 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 )
 
 func TestMain(t *testing.T) {
+	suite.Run(t, new(MainTestSuite))
+}
+
+type MainTestSuite struct {
+	suite.Suite
+	ExitCode int
+}
+
+func (s *MainTestSuite) SetupTest() {
+	s.ExitCode = -1
+	exit = s.mockExit
+}
+
+func (s *MainTestSuite) TearDownTest() {
+	exit = os.Exit
+}
+
+func (s *MainTestSuite) mockExit(code int) {
+	s.ExitCode = code
+}
+
+func (s *MainTestSuite) TestMain() {
+	check := s.Require()
+	check.Equal(-1, s.ExitCode)
 	main()
+	check.Equal(1, s.ExitCode)
 }
