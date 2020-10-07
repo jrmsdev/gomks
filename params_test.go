040c560f095b94341ab97fe5bf9c912c16b2352c
Jeremías <jrmsdev@gmail.com>
2020-10-07 12:10:32 -0300

    test args and params types

--- params_test.go
+++ params_test.go
@@ -2,3 +2,19 @@
 // See LICENSE file.
 
 package gomks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestArgsNew(t *testing.T) {
+	check := require.New(t)
+	check.IsType(map[string]string{}, ArgsNew())
+}
+
+func TestParamsNew(t *testing.T) {
+	check := require.New(t)
+	check.IsType(map[string]string{}, ParamsNew())
+}
