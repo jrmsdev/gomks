62e33fe1584d763e11d6389d6c38cba9e4831d65
Jeremías <jrmsdev@gmail.com>
2020-10-19 19:58:28 -0300

    test tpl_render vfs errors

--- tpl_test.go
+++ tpl_test.go
@@ -18,3 +18,27 @@ func TestTplBuild(t *testing.T) {
 	diffCheck(t, filepath.Join("testdata", "tpl", "site"),
 		filepath.Join("testdata", "_tmp", "tpl", "out"))
 }
+
+func TestTplRenderVfsErrors(t *testing.T) {
+	check := require.New(t)
+	tpl := TplParse("testdata/tpl/template/page.html")
+	render := func() {
+		TplRender("testdata/tpl/content/_index.html",
+			"testdata/_tmp/tpl/site-index.html",
+			tpl, ParamsNew())
+	}
+	defer setNativeFS()
+	// write error
+	setMockFS("WithWriteError")
+	check.PanicsWithError("mock write error", render)
+	// mkdir error
+	setMockFS("WithMkdirError")
+	check.PanicsWithError("mock mkdir error", render)
+	// glob path error
+	setMockFS("WithGlobError")
+	check.PanicsWithError("mock glob error", render)
+	// abs path error
+	setMockFS("WithPathError")
+	fs.(*mockFS).PathErrorCall = 2
+	check.PanicsWithError("mock abspath error: 2", render)
+}
